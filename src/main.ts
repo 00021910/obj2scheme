c0e4e8b5d5c0de246ddbed735fea2fd03d460536
AzizbekTheDev <azizbekmasharibov@gmail.com>
2021-10-08 05:30:52 +0500

    added array support

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
-import { obj2scheme } from "./core";
+export { obj2scheme } from "./core";
 
-let obj = {
-    a: [1, 2, 3],
-    b: [
-        {name: "x", value:1},
-        {name: "y", value:2}
-    ],
-    c: "hello world"
-}
-
-console.log(obj2scheme(obj));
+/** @usage
+ * let jsonobj = { ... }
+ * let scheme = obj2scheme(jsonobj)
+ */
